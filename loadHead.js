20b77167ac255fb34f161c073d42e61c1ccd9f84
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2021-11-05 19:24:22 +0100

    Head is now loaded via loadHead.js

---
+++ loadHead.js
@@ -0,0 +1,11 @@
+document.write(`
+
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>Kotmasters</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="shortcut icon" href="https://staubtornado.github.io/kotmasters/icon.ico" />
+<link rel="stylesheet" href="https://github.com/Staubtornado/kotmasters/blob/main/stylesheet.css">
+
+`);
