1d29d4e0312781dcce0c308947840f52c85ebbd8
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2021-11-05 16:55:01 +0100

    Added java script for continuity

---
+++ loadHeader.js
@@ -0,0 +1,16 @@
+document.write(`
+
+<div>
+    <img class="logo" src="https://staubtornado.github.io/kotmasters/Logo.png" alt="logo">
+    <a class="logo__font" href="">Kotmasters</a>
+  </div>
+  
+    <nav>
+      <ul class="nav__links">
+        <li><a href="">Services</a></li>
+        <li><a href="https://staubtornado.github.io/kotmasters/content/pages/about.html">About</a></li>
+        <li><a href="">Projects</a></li>
+    </nav>
+    <a class="cta" href=""><button>Contact</button></a>
+
+`);
